349e73be2e3001b9b99c35dfb9b38bf8e2882795
atif <ratul0947@gmail.com>
2022-02-28 22:30:11 +0600

    react navigation implementation with props

--- App.js
+++ App.js
@@ -1,76 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Image } from "react-native-web";
+import { StyleSheet, View } from "react-native";
 import Home from "./src/screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
 	return (
-		<View style={styles.mainContainer}>
-			<View style={styles.homeTop}>
-				<Image
-					style={styles.headerImage}
-					source={require("./assets/pexels-emily-ranquist-1205651.jpg")}
-					resizeMode='content'
-				/>
-				<Text style={styles.mainHeader}>hello To</Text>
-				<Text
-					style={[
-						styles.mainHeader,
-						{
-							fontSize: 33,
-							color: "#888",
-							marginTop: 0,
-						},
-					]}
-				>
-					Atif
-				</Text>
-				<Text style={styles.paraStyle}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				</Text>
-			</View>
+		<View>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName='Home'>
+					{/* <Stack.Screen name='Home' component={Home} /> */}
+					<Stack.Screen name='Home'>
+						{(props) => <Home {...props} userName={"atif aslam"} />}
+					</Stack.Screen>
+				</Stack.Navigator>
+			</NavigationContainer>
+			<StatusBar style='auto' />
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	mainContainer: {
-		height: "100vh",
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "space-between",
-		paddingHorizontal: 20,
-		backgroundColor: "#fff",
-		textAlign: "center",
-	},
-	homeTop: {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		paddingHorizontal: 10,
-	},
-	headerImage: {
-		height: undefined,
-		width: "100%",
-		aspectRatio: 1,
-		display: "flex",
-		alignItems: "stretch",
-		marginTop: 50,
-		borderRadius: 10,
-	},
-	mainHeader: {
-		fontSize: 30,
-		color: "#344055",
-		textTransform: "uppercase",
-		marginTop: 20,
-	},
-	paraStyle: {
-		textAlign: "left",
-		fontSize: 20,
-		color: "#7d7d7d",
-		marginTop: 30,
-		paddingBottom: 50,
-		lineHeight: 25,
-	},
-});
+const styles = StyleSheet.create({});
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,77 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image } from "react-native-web";
 
-const Home = () => {
+const Home = ({userName}) => {
 	return (
-		<View>
-			<Text>Home</Text>
+		<View style={styles.mainContainer}>
+			<View style={styles.homeTop}>
+				<Image
+					style={styles.headerImage}
+					source={require("../../assets/pexels-emily-ranquist-1205651.jpg")}
+					resizeMode='content'
+				/>
+				<Text style={styles.mainHeader}>hello To</Text>
+				<Text
+					style={[
+						styles.mainHeader,
+						{
+							fontSize: 33,
+							color: "#888",
+							marginTop: 0,
+						},
+					]}
+				>
+					{userName}
+				</Text>
+				<Text style={styles.paraStyle}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</Text>
+			</View>
 		</View>
 	);
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	mainContainer: {
+		height: "100vh",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-between",
+		paddingHorizontal: 20,
+		backgroundColor: "#fff",
+		textAlign: "center",
+	},
+	homeTop: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingHorizontal: 10,
+	},
+	headerImage: {
+		height: undefined,
+		width: "100%",
+		aspectRatio: 1,
+		display: "flex",
+		alignItems: "stretch",
+		marginTop: 50,
+		borderRadius: 10,
+	},
+	mainHeader: {
+		fontSize: 30,
+		color: "#344055",
+		textTransform: "uppercase",
+		marginTop: 20,
+	},
+	paraStyle: {
+		textAlign: "left",
+		fontSize: 20,
+		color: "#7d7d7d",
+		marginTop: 30,
+		paddingBottom: 50,
+		lineHeight: 25,
+	},
+});
