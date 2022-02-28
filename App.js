02cee59b9a303c68c867ad17f0769fcf3fa8263b
atif <ratul0947@gmail.com>
2022-02-28 21:42:00 +0600

    home page

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native-web";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.mainContainer}>
+			<View style={styles.homeTop}>
+				<Image
+					style={styles.headerImage}
+					source={require("./assets/pexels-emily-ranquist-1205651.jpg")}
+					resizeMode='content'
+				/>
+				<Text style={styles.mainHeader}>hello To</Text>
+				<Text
+					style={[
+						styles.mainHeader,
+						{
+							fontSize: 33,
+							color: "#888",
+							marginTop: 0,
+						},
+					]}
+				>
+					Atif
+				</Text>
+				<Text style={styles.paraStyle}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	mainContainer: {
+		height: "100vh",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-between",
+		paddingHorizontal: 20,
+		backgroundColor: "#fff",
+		textAlign: "center",
+	},
+	homeTop: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingHorizontal: 10,
+	},
+	headerImage: {
+		height: undefined,
+		width: "100%",
+		aspectRatio: 1,
+		display: "flex",
+		alignItems: "stretch",
+		marginTop: 50,
+		borderRadius: 10,
+	},
+	mainHeader: {
+		fontSize: 30,
+		color: "#344055",
+		textTransform: "uppercase",
+		marginTop: 20,
+	},
+	paraStyle: {
+		textAlign: "left",
+		fontSize: 20,
+		color: "#7d7d7d",
+		marginTop: 30,
+		paddingBottom: 50,
+		lineHeight: 25,
+	},
 });
